b8ed10419a13f4c985c200933143f89d3b11f962
aoihigashimoto <higashimotoaoi@higashimotoaoinoMacBook-Pro.local>
2023-06-07 20:42:36 +0900

    first commit

---
+++ Random_WorldTime/Random_WorldTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Random_WorldTimeApp.swift
+//  Random_WorldTime
+//
+//  Created by ひがしもとあおい on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct Random_WorldTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
