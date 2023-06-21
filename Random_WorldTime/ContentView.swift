bd74722775ab8f3071cf8aadac9170158dbf1594
aoihigashimoto <higashimotoaoi@higashimotoaoinoMacBook-Pro.local>
2023-06-21 18:39:21 +0900

    ContentViewからMainViewに遷移する実装を完了

--- Random_WorldTime/ContentView.swift
+++ Random_WorldTime/ContentView.swift
@@ -4,19 +4,38 @@
 //
 
 import SwiftUI
-
 import GoogleMobileAds
 
 struct ContentView: View {
     @Environment(\.horizontalSizeClass) private var horizontalSizeClass
     @Environment(\.verticalSizeClass) private var verticalSizeClass
     @EnvironmentObject private var sceneDelegate: MySceneDelegate
-
+    
+    //MainViewに遷移するための状態変数
+    @State private var isMainViewPresented = false
+    
     var body: some View {
         VStack {
-            Text("Ad")
+            //Text("Ad")
             //アプリ説明のTextFieldの実装
+            Text("アプリの説明  \n アプリの説明 \n アプリの説明 \n アプリの説明 \n アプリの説明 \n アプリの説明 \n アプリの説明")
+                .font(.subheadline)
+                .foregroundColor(.gray)
+                .padding()
             //アプリを始めるButtonを実装して、次の画面(MainView)に遷移させるプログラム
+            Button("アプリを始める！") {
+                isMainViewPresented = true
+            }
+            //ここを
+            .font(.title)
+            .fontWeight(.bold)
+            .frame(width: 280, height: 60,alignment: .center)
+            .foregroundColor(.white)
+            .background(Color.green)
+            .cornerRadius(15,antialiased: true)
+            .fullScreenCover(isPresented: $isMainViewPresented) {
+                MainView()
+            }
             Spacer()
             if let vc = sceneDelegate.window?.rootViewController {
                 switch (verticalSizeClass, horizontalSizeClass) {
